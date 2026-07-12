ee5bedb365e59389ef0a66b522257acebae78e76
imam <emamexp2@gmail.com>
2026-07-12 17:00:53 +0900

    category set

--- src/helpers/productCategory.jsx
+++ src/helpers/productCategory.jsx
@@ -1,14 +1,14 @@
 const productCategory = [
   { id: 1, label: "Men", value: "Men" },
-  { id: 2, label: "Men's Accessories", value: "MensAccessories" },
+  { id: 2, label: "Men's Accessories", value: "Men's Acces" },
   { id: 3, label: "Women", value: "Women" },
-  { id: 4, label: "Japanese Beauty Care", value: "JapaneseBeautyCare" },
-  { id: 5, label: "Jewelry & Watches", value: "JewelryWatches" },
-  { id: 6, label: "Home & Kitchen", value: "HomeKitchen" },
-  { id: 7, label: "Mobile Accessories", value: "MobileAccessories" },
+  { id: 4, label: "Japanese Beauty", value: "Japanese Beauty" },
+  { id: 5, label: "Jewelry & Watches", value: "Jewelry & Watches" },
+  { id: 6, label: "Home & Kitchen", value: "Home & Kitchen" },
+  { id: 7, label: "Mobile Acces", value: "Mobile Acces" },
   { id: 8, label: "Bags", value: "Bags" },
-  { id: 9, label: "Kids & Baby", value: "KidsBaby" },
-  { id: 10, label: "Toys & Games", value: "ToysGames" },
+  { id: 9, label: "Kids & Baby", value: "Kids & Baby" },
+  { id: 10, label: "Toys & Games", value: "Toys & Games" },
 ];
 
 export default productCategory;
