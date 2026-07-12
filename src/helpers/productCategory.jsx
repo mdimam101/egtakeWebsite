1da46dda505a7706662910df4f66e811d2cf04fb
imam <emamexp2@gmail.com>
2026-07-12 16:54:49 +0900

    category set

--- src/helpers/productCategory.jsx
+++ src/helpers/productCategory.jsx
@@ -1,15 +1,14 @@
 const productCategory = [
-    {id : 1, label : 'Women', value : 'Women'},
-    {id : 2, label : 'Jewelry', value : 'Jewelry'},
-    {id : 3, label : 'Men', value : 'Men'},
-    {id : 4, label : 'Home', value : 'Home'},
-    {id : 5, label : 'Kitchen', value : 'Kitchen'},
-    {id : 6, label : 'MobileAccs', value : 'Mobile Accs'},
-    {id : 7, label : 'Bags', value : 'Bags'},
-    {id : 8, label : 'Beauty', value : 'Beauty'},
-    {id : 9, label : 'Baby', value : 'Baby'},
-    {id : 10, label : 'Toys', value : 'Toys'},
-    
-]
+  { id: 1, label: "Men", value: "Men" },
+  { id: 2, label: "Men's Accessories", value: "MensAccessories" },
+  { id: 3, label: "Women", value: "Women" },
+  { id: 4, label: "Japanese Beauty Care", value: "JapaneseBeautyCare" },
+  { id: 5, label: "Jewelry & Watches", value: "JewelryWatches" },
+  { id: 6, label: "Home & Kitchen", value: "HomeKitchen" },
+  { id: 7, label: "Mobile Accessories", value: "MobileAccessories" },
+  { id: 8, label: "Bags", value: "Bags" },
+  { id: 9, label: "Kids & Baby", value: "KidsBaby" },
+  { id: 10, label: "Toys & Games", value: "ToysGames" },
+];
 
-export default productCategory
+export default productCategory;
