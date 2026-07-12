3617c66071b533de2fa765650bd613369a4a0cf9
imam <emamexp2@gmail.com>
2026-07-12 17:09:34 +0900

    category focus solve

--- src/helpers/productCategory.jsx
+++ src/helpers/productCategory.jsx
@@ -1,6 +1,6 @@
 const productCategory = [
   { id: 1, label: "Men", value: "Men" },
-  { id: 2, label: "Men's Accessories", value: "Men's Acces" },
+  { id: 2, label: "Men's Acces", value: "Men's Acces" },
   { id: 3, label: "Women", value: "Women" },
   { id: 4, label: "Japanese Beauty", value: "Japanese Beauty" },
   { id: 5, label: "Jewelry & Watches", value: "Jewelry & Watches" },
